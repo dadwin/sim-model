74482ef60a8364513301c454bf6c6f94cdb41584
dadwin <yzaytaev@andrew.cmu.edu>
2015-10-20 20:14:44 +0300

    Final commit for identifying channels in network

--- SimpleNetwork/src/Switch.cc
+++ SimpleNetwork/src/Switch.cc
@@ -26,44 +26,42 @@ void Switch::initialize()
     const double maxCapacity = par("maxCapacity");
     Resource* resource = new SwitchResource(this, 0, maxCapacity); // TODO what about id? can it be 'this' pointer?
     network.registerResource(resource);
-#if 0
-    for (cModule::GateIterator it(this); !it.end(); it++) {
-        cGate* gate = it();
 
-        assert(gate->isVector() == true);
+    // identifying channels as resources
+    // we iterate through all OUTPUT gates and those which have underlying channels,
+    // since some of them have not - e.g. case of connection between rack switch and rack itself.
+    for (GateIterator it(this); !it.end(); it++) {
+        auto gate = it();
 
-        // TODO log something special here.
-//        ev << gate->getFullName() << gate->getChannel() << gate->getFullPath() << gate->isConnectedOutside() << gate->isConnectedInside() << endl;
-
-        auto ch = gate->getChannel();
-
-        if (ch != nullptr) {
-            // if a gate has no underlying channel
-            // then this gate is not interesting for us from link point of view
-            const double maxCapacity = ch->par("maxCapacity");
-            Resource* resource = new LinkResource(ch, 0, maxCapacity); // TODO what about id? can it be 'this' pointer?
-            network.registerResource(resource);
-        } else {
-            //throw cRuntimeError("null pointer of channel");
-        }
-    }
-#endif
-    for (cModule::GateIterator it(this); !it.end(); it++) {
-        cGate* gate = it();
-
-        assert(gate->isVector() == true);
         if (gate->getType() != cGate::OUTPUT)
             continue;
-        auto ch = gate->getChannel();
 
-        if (ch == nullptr)
-            continue;
-        const double maxCapacity = ch->par("maxCapacity");
-        Resource* resource = new LinkResource(ch, 0, maxCapacity); // TODO what about id? can it be 'this' pointer?
-        ev << (void*) ch << " " << (void*) resource << endl;
+        auto channel = gate->getChannel();
+        if (channel == nullptr) {
+            //continue;
+            auto nextGate = gate->getNextGate();
+            if (nextGate == nullptr) {
+                continue;
+                // I expect only one case when nextGate is null:
+                // for switch of storage level. This switch may be connected to nowhere
+                // (at least for beginning). However when model will become complete
+                // this case will be vanished and exception should be here:
+                // throw cRuntimeError("nextGate should be non-null");
+            }
+
+            channel = nextGate->getChannel();
+            if (channel == nullptr) {
+                // I assume that for nextGate channel must be no-null.
+                // It's implied from network topology.
+                // Probably I can relaxed this requirement, but I prefer to know about it explicitly
+                // rather than via debugging.
+                throw cRuntimeError("channel under nextGate should be non-null");
+            }
+        }
+        const double maxCapacity = channel->par("maxCapacity");
+        Resource* resource = new LinkResource(channel, 0, maxCapacity); // TODO what about id? can it be 'this' pointer?
         network.registerResource(resource);
     }
-    ev << network.resources.size() << endl;
 }
 
 void Switch::handleMessage(cMessage *msg)
