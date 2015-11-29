03ce4ebae11ef63ae94481a5ae8de6b807f1c3f5
dadwin <yzaytaev@andrew.cmu.edu>
2015-11-29 16:26:46 +0300

    Added options to run tests with Valgrind Cleaned up runtest.sh

--- storage-simulation/tests/runtest.sh
+++ storage-simulation/tests/runtest.sh
@@ -5,21 +5,29 @@
 #
 MAKE=make
 
+# start with valgrind if the first argument is --valgrind
+VALGRIND=""
+VALGRIND_OPTIONS="-v --tool=memcheck --trace-children=yes --leak-check=yes --show-reachable=no --leak-resolution=high --num-callers=40 --freelist-vol=4000000"
+
+if [ "$1" = "--valgrind" ] ; then
+   echo "##### Running with Valgrind! ######"
+   VALGRIND="valgrind $VALGRIND_OPTIONS"
+   shift
+fi
+
 #export NEDPATH="../../../src;"
 
 TESTFILES=$*
 if [ "x$TESTFILES" = "x" ]; then TESTFILES='*.test'; fi
 if [ ! -d work ];  then mkdir work; fi
-#rm -rf work/lib
-#cp -pPR lib work/       # OSX dos not support cp -a
-#EXTRA_INCLUDES="-I../../src/"
-#EXTRA_INCLUDES="$EXTRA_INCLUDES -I./lib "
+
 opp_test gen $OPT -v $TESTFILES || exit 1
+
 echo
 (cd work; opp_makemake -f --deep -L../../src -lstorage-simulation -P . --no-deep-includes -I../../src; $MAKE) || exit 1
 echo
 #env
-opp_test run -v $OPT $TESTFILES || exit 1
+$VALGRIND opp_test run -v $OPT $TESTFILES || exit 1
 echo
 echo Results can be found in ./work
 
